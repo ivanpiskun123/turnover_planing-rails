697529c0f0a191a18e5ff4d0627e748d49b93b66
ivanpiskun123 <ivan.piskun.s@gmail.com>
2021-08-03 06:32:50 +0300

    Add sales sum method

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,5 +1,8 @@
 class Month < ApplicationRecord
 
+  # scope :with_juice, -> { where("juice > 0") }
+  # Fruit.with_juice.with_round_shape.first(3)
+
   before_save :date_to_month_start
 
   has_many :sales
@@ -12,10 +15,16 @@ class Month < ApplicationRecord
 
   validates :price_index, numericality: { greater_than_or_equal_to: 0.00001, less_than_or_equal_to: 99999999999}
 
+  def sales_sum
+    self.sales.to_a.sum(&:total_sum)
+  end
+
   private
 
   def date_to_month_start
     self.date = self.date.end_of_month
   end
+
+
 
 end
